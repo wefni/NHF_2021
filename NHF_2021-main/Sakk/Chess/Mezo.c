92b102d4b261256f554ed73abdf5f00044b77b7f
Daweed <47455954+wefni@users.noreply.github.com>
2021-11-14 21:11:26 +0100

    Add files via upload

---
+++ NHF_2021-main/Sakk/Chess/Mezo.c
@@ -0,0 +1,6 @@
+#include "Mezo.h"
+
+void poz(int x,int y,Mezo *m){
+        m->x = x;
+        m->y = y;     //A kapott m Mezo-nek beallitjuk a kapott x,y koordinatajat
+}
---
+++ NHF_2021-main/Sakk/Chess/Mezo.h
@@ -0,0 +1,30 @@
+#ifndef MEZO
+#define MEZO
+#include "debugmalloc.h"
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL2_gfxPrimitives.h>
+
+typedef enum Tipus{
+    kiraly,vezer,bastya,futo,lo,paraszt,nincs     //A babuk tipusai, illetve egy nincs tipus az ures mezok megkulonboztetesere
+}Tipus;
+
+typedef enum Babuszin{
+    feher,fekete,nincs2   //Szinek, ugyanigy a nincs2 az ures mezore vonatkozik
+}Babuszin;
+
+typedef struct Babu{
+    Tipus tipus;
+    Babuszin szin;
+}Babu;
+
+typedef struct Mezo{
+    Babu babu;         //Egy mezo alapvetoen egy babubol es egy x,y koordinatabol all
+    int x,y;
+}Mezo;
+
+enum {MERET=52};     //A babuknak a forras fajlban levo merete
+
+void p(int x,int y,Mezo *m);
+
+#endif // MEZO
